1eec2f6f9a3943aac27262b6947aa7efa4e127e3
Subha <subhapdas@aptsoftware.com>
2018-12-10 13:07:09 +0530

    Add jpa, mysql create table

---
+++ src/main/java/com/example/demo/Domain/BaseDomain.java
@@ -0,0 +1,57 @@
+package com.example.demo.Domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+public class BaseDomain implements Serializable {
+
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "isActive", columnDefinition = "boolean default true")
+    private boolean isActive = true;
+
+    @Temporal(value = TemporalType.DATE)
+    @Column(name = "createDate")
+    private Date createDate = new Date();
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(name = "updateDate")
+    private Date updateDate = new Date();
+
+    public BaseDomain(Long id, boolean isActive, Date createDate, Date updateDate) {
+        this.id = id;
+        this.isActive = isActive;
+        this.createDate = createDate;
+        this.updateDate = updateDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+}
