680dfce0fee110622c8429205d89e777e39f9c70
Subha <subhapdas@aptsoftware.com>
2018-12-12 17:04:19 +0530

    Service Modified insert data using constructor

--- src/main/java/com/example/demo/RepositoryOrDao/UserRepository.java
+++ src/main/java/com/example/demo/RepositoryOrDao/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.RepositoryOrDao;
 
 import com.example.demo.Domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
 }
